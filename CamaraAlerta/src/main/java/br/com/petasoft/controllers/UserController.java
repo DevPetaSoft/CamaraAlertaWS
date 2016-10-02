a168ee71dedb6211cc94b910de09adea3036006d
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2016-10-02 17:44:56 -0300

    Login com facebook e g+

--- CamaraAlerta/src/main/java/br/com/petasoft/controllers/UserController.java
+++ CamaraAlerta/src/main/java/br/com/petasoft/controllers/UserController.java
@@ -1,18 +1,24 @@
 package br.com.petasoft.controllers;
 
+import org.apache.log4j.Logger;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.boilerplate.NoCache;
 import br.com.petasoft.dao.CidadeDAO;
+import br.com.petasoft.dao.UsuarioDAO;
+import br.com.petasoft.model.Cidadao;
 import br.com.petasoft.model.Cidade;
 
 @Controller
 @Path("/user")
 public class UserController extends br.com.caelum.vraptor.boilerplate.AbstractController {
-
+	final static Logger logger = Logger.getLogger(UserController.class);
+	
 	public static CidadeDAO cDao = new CidadeDAO();
+	public static UsuarioDAO uDao = new UsuarioDAO();
 	
 /*	@Get("/teste")
 	@NoCache
@@ -29,4 +35,37 @@ public class UserController extends br.com.caelum.vraptor.boilerplate.AbstractCo
 		cDao.Salvar(cidade);
 		this.success(cidade);
 	}
+	
+	/*
+	 *  Requisição get para realizar o login via facebook,
+	 *  Se encontrar um usuário pelo token, ele retorna o usuário,
+	 *  caso ao contrário, é enviada uma flag para realizar um novo cadastro.
+	 */
+	@Get("/facebookLogin")
+	public void facebookLogin(String token){
+		Cidadao cidadao = new Cidadao();
+		cidadao = uDao.buscarUsuarioPorFacebookToken(token);
+		if(cidadao == null){
+			this.success("novoCadastro");
+		}else{
+			this.success(cidadao);
+		}
+	}
+	
+	/*
+	 *  Requisição get para realizar o login via facebook,
+	 *  Se encontrar um usuário pelo token, ele retorna o usuário,
+	 *  caso ao contrário, é enviada uma flag para realizar um novo cadastro.
+	 */
+	@Get("/gplusLogin")
+	public void gplusLogin(String token){
+		Cidadao cidadao = new Cidadao();
+		cidadao = uDao.buscarUsuarioPorGPlusToken(token);
+		if(cidadao == null){
+			this.success("novoCadastro");
+		}else{
+			this.success(cidadao);
+		}
+	}
+	
 }
---
+++ CamaraAlerta/src/main/java/br/com/petasoft/dao/UsuarioDAO.java
@@ -0,0 +1,43 @@
+package br.com.petasoft.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.petasoft.infra.CriadorDeSession;
+import br.com.petasoft.model.Cidadao;
+import br.com.petasoft.model.Vereador;
+
+public class UsuarioDAO {
+	private final Session session;
+	
+	/*
+	 * Construtor do DAO do usuário, é inicializado uma nova sessão
+	 * */
+	public UsuarioDAO(){
+		this.session = CriadorDeSession.getSession();
+	}
+	
+	/*
+	 * Consulta de um cidadao pelo tokent deo facebook
+	 * */
+	public Cidadao buscarUsuarioPorFacebookToken(String token){
+		Criteria criteria = this.session.createCriteria(Cidadao.class);
+		criteria.add(Restrictions.eq("tokenFacebook", token));
+		criteria.add(Restrictions.eq("deleted", false));
+		Cidadao cidadao = (Cidadao) criteria.uniqueResult();
+		return cidadao;
+	}
+	
+	/*
+	 * Consulta de um cidadao pelo tokent deo facebook
+	 * */
+	public Cidadao buscarUsuarioPorGPlusToken(String token){
+		Criteria criteria = this.session.createCriteria(Cidadao.class);
+		criteria.add(Restrictions.eq("tokenGPlus", token));
+		criteria.add(Restrictions.eq("deleted", false));
+		Cidadao cidadao = (Cidadao) criteria.uniqueResult();
+		return cidadao;
+	}
+}
